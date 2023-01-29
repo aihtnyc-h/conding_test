22873af8a6084bdad48f8ae1ceb78507ec86a47a
aihtnyc_h <feiver@naver.com>
2023-01-30 00:09:11 +0900

    1월 30일 프로그래머스 LV.1문제 수정

--- LV1/H12917.java
+++ LV1/H12917.java
@@ -1,4 +1,14 @@
 package LV1;
 
+import java.util.Arrays;
+
 public class H12917 {
+    public static String solution(String s) {
+        String answer = "";
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
 }
--- LV1/H86491.java
+++ LV1/H86491.java
@@ -1,24 +1,35 @@
 package LV1;
 
+import java.util.Arrays;
+
 public class H86491 {
 
-    public static int solution(int[][] sizes){
+    public static int solution(int[][] sizes) {
         int width = 0;
         int height = 0;
-        for (int i = 0; i <sizes.length ; i++) {
+        for (int i = 0; i < sizes.length; i++) {
             int max = Math.max(sizes[i][0], sizes[i][1]);
             int min = Math.min(sizes[i][0], sizes[i][1]);
 
-            if(max > width) width = max;
-            if(min > height) height = min;
+            if (max > width) width = max;
+            if (min > height) height = min;
         }
 
         return width * height;
     }
 
     // 결과값 뭐로 넣을지 고민하기
-    public static void main(String args){
+    public static void main(String[] args){
 
         System.out.println();
+
     }
 }
+/*
+오늘도 실패...
+public static void main(String[] args){
+        int[] width = new int[0];
+        int[] height = new int[0];
+        int[] sizes = width * height;
+        System.out.println(sizes);
+ */
