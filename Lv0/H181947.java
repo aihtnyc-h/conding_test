7daa152fd8e217c0e9d9ce41142e96b3b296e7fb
aihtnyc-h <feiver@naver.com>
2023-05-02 22:50:14 +0900

    5월 2일 프로그래머스 알고리즘 문제풀이

---
+++ Lv0/H181947.java
@@ -0,0 +1,18 @@
+package Lv0;
+/*
+두 정수 a, b가 주어질 때 다음과 같은 형태의 계산식을 출력하는 코드를 작성해 보세요.
+
+a + b = c
+ */
+import java.util.Scanner;
+
+public class H181947 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = a + b;
+
+		System.out.println(a + " + " + b + " = " + c);
+	}
+}
