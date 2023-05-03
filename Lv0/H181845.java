3f0bfffda297abbdab84b2bcebd2f4e63832b624
aihtnyc-h <feiver@naver.com>
2023-05-03 21:33:45 +0900

    5월 3일 프로그래머스 알고리즘 문제풀이

---
+++ Lv0/H181845.java
@@ -0,0 +1,15 @@
+package Lv0;
+/*
+정수 n이 주어질 때, n을 문자열로 변환하여 return하도록 solution 함수를 완성해주세요.
+ */
+public class H181845 {
+	public static String solution(int n) {
+		String s = String.valueOf(n);
+		return s;
+	}
+	public static void main(String[] args){
+		int n = 123;
+		System.out.println(solution(n));
+	}
+
+}
