559e45299adfe966787801d6e8e27aeef5e0f5b6
aihtnyc-h <feiver@naver.com>
2023-05-03 21:33:42 +0900

    5월 3일 프로그래머스 알고리즘 문제풀이

---
+++ Lv0/H181843.java
@@ -0,0 +1,11 @@
+package Lv0;
+
+public class H181843 {
+	public int solution(String my_string, String target) {
+		if (my_string.contains(target)) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+}
