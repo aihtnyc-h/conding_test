18d9e3e2476ff702fe196bd6b64082831859de36
aihtnyc-h <feiver@naver.com>
2023-05-09 13:15:44 +0900

    5월 9일 프로그래머스 알고리즘 문제풀이

---
+++ Lv0/H181952.java
@@ -0,0 +1,11 @@
+package Lv0;
+// 문자열 str이 주어질 때, str을 출력하는 코드를 작성해 보세요.
+import java.util.Scanner;
+
+public class H181952 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+		System.out.println(a);
+	}
+}
