811b3e3d58c11d9c74d5d16928c165459a29e569
aihtnyc-h <feiver@naver.com>
2023-05-11 10:58:08 +0900

    5월 11일 프로그래머스 알고리즘 문제풀이

---
+++ Lv0/H181923.java
@@ -0,0 +1,29 @@
+package Lv0;
+/*
+정수 배열 arr와 2차원 정수 배열 queries이 주어집니다. queries의 원소는 각각 하나의 query를 나타내며, [s, e, k] 꼴입니다.
+
+각 query마다 순서대로 s ≤ i ≤ e인 모든 i에 대해 k보다 크면서 가장 작은 arr[i]를 찾습니다.
+
+각 쿼리의 순서에 맞게 답을 저장한 배열을 반환하는 solution 함수를 완성해 주세요.
+단, 특정 쿼리의 답이 존재하지 않으면 -1을 저장합니다.
+ */
+public class H181923 {
+	public int[] solution(int[] arr, int[][] queries) {
+		int[] answer = new int[queries.length];
+
+		for (int i = 0; i < queries.length; i++) {
+			int s = queries[i][0];
+			int e = queries[i][1];
+			int k = queries[i][2];
+
+			int min = Integer.MAX_VALUE;
+			for (int j = s; j <= e; j++) {
+				if (arr[j] > k && arr[j] < min) {
+					min = arr[j];
+				}
+			}
+			answer[i] = min == Integer.MAX_VALUE ? -1 : min;
+		}
+		return answer;
+	}
+}
